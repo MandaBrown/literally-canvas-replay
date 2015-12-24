2e0c73e28abad8a9e0790dd3379de70df3d78e8c
Mandy Brown <mfarmer@thinkthroughmath.com>
2015-12-24 17:26:32 -0500

    Initialize frame controls

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var LC = require('./vendor/literallycanvas-core.min.js');
 var Actions = require('./actions/actions.js');
+var FrameControls = require('./controls/frame_controls.js');
 
 module.exports = LiterallyCanvasReplay;
 
@@ -8,8 +9,12 @@ function LiterallyCanvasReplay(opts){
 
   var literalCanvas = LC.init(opts.canvasElement, opts.lcOptions);
   var canvasActions = new Actions(literalCanvas);
+  var frameControls = new FrameControls({
+    canvasElement: opts.canvasElement
+  });
 
   var processActions = function(actions) {
+    frameControls.loadActions(actions.length);
     actions.forEach(function(action){
       canvasActions.execute(action.action, action);
     });
